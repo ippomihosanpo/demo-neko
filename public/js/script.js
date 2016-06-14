11853a2972a6028e5d4c87c559a36539f8718aa3
ashida <loklik.patricia.dyt@gmail.com>
2016-06-14 20:11:03 +0900

    調整中

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,22 @@
 $(function(){
   $('#execute').click(
     function(){
+      var data = {
+        "age": $('#age').val(),
+        "gender": $('#gender').val(),
+        "height": $('#height').val(),
+        "weight": $('#weight').val(),
+        "meal_rate": $('#meal_rate').val(),
+        "motion_rate": $('#motion_rate').val(),
+        "crunch_rate": $('#crunch_rate').val()
+      }
       $.ajax({
-        url: 'https://e3q07zphak.execute-api.us-east-1.amazonaws.com/prod/predict',
         type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        headers: {
-          'x-api-key': 'DGswIXFuXq4pTlxhCjlgf9U2dvJrJThJ2oZi4ppY'
-        },
-        data: {
-          "age": $('#age').val(),
-          "gender": $('#gender').val(),
-          "height": $('#height').val(),
-          "weight": $('#weight').val(),
-          "meal_rate": $('#meal_rate').val(),
-          "motion_rate": $('#motion_rate').val(),
-          "crunch_rate": $('#crunch_rate').val()
+        url: 'https://6q09kuhuic.execute-api.us-east-1.amazonaws.com/prod/predict',
+        data: JSON.stringify(data),
+        success: function(data) {
+          alert(data);
         }
-      }).done(function(data) {
-        alert(data);
-      }).fail(function(data) {
-        console.log(data);
       });
     }
   );

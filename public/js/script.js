cd1e7081e85c2885c37b9920df51d20a26cd25d6
ashida <loklik.patricia.dyt@gmail.com>
2016-06-15 01:43:21 +0900

    調整

--- public/js/script.js
+++ public/js/script.js
@@ -14,10 +14,28 @@ $(function(){
         type: 'POST',
         url: 'https://6q09kuhuic.execute-api.us-east-1.amazonaws.com/prod/predict',
         data: JSON.stringify(data),
-        success: function(data) {
-          alert(data);
-        }
-      });
+      }).done(function(data) {
+          result = data.Prediction.predictedValue.toFixed(1)
+          if(result <= -1.0) {
+            neko_size = 's'
+          } else if(result <= -0.5) {
+            neko_size = 'm'
+          } else if(result <= -0.1) {
+            neko_size = 'l'
+          } else {
+            neko_size = ''
+          }
+          $('#neko').addClass('shake-little shake-constant')
+          $('#neko').removeClass('s m l').addClass(neko_size)
+          setTimeout(function(){
+            $('#neko').removeClass('shake-little shake-constant')
+            setTimeout(function(){
+              $('#result').text(result * 2 + 'kg');
+            },1000);
+          },6000);
+      }).fail(function(data) {
+          alert('error');
+      })
     }
   );
 });

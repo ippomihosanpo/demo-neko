45a2cc5b2249280a743423f7b210355d5327538b
moritoraman <atlas.echoes99@gmail.com>
2016-06-15 21:23:38 +0900

    ネコの位置調整とローディングエフェクト追加

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,37 @@
+// Loading
+var COUNT = 1
+var LOADING_FLAG = false
+function callLoading(){
+  $('#loading').fadeIn(function(){
+    $('#loading-image').addClass("rotateAnim");
+    countDate();
+  });
+}
+function countDate(){
+  var interval = setInterval(function(){
+    if(COUNT < 30){
+      COUNT++
+      $('#loading-image').find("p").html(COUNT);
+    }else{
+      if(LOADING_FLAG == true){
+        LOADING_FLAG = false
+        endLoading();
+        clearInterval(interval);
+      }
+    }
+  }, 50);
+}
+function endLoading(){
+  COUNT = 31
+  $('#loading-image').find("p").html(COUNT);
+  setTimeout(function(){
+    $('#loading').fadeOut(function(){
+      COUNT = 1
+      $('#loading-image').find("p").html(COUNT);
+    });
+  },300);
+}
+
 $(function(){
   $('#execute').click(
     function(){
@@ -14,8 +48,14 @@ $(function(){
         type: 'POST',
         url: 'https://6q09kuhuic.execute-api.us-east-1.amazonaws.com/prod/predict',
         data: JSON.stringify(data),
+        beforeSend: function(){
+          callLoading();
+        },
         success: function(data) {
           alert(data);
+        },
+        complete: function(){
+          LOADING_FLAG = true
         }
       });
     }
